8e3b45286cf30b27eb58c6258ecf0799aa24519a
pengwenhao <pengwenhao@aliyun.com>
2025-08-05 11:15:43 +0800

    feat:init Application

---
+++ src/main/java/com/coderpwh/Application.java
@@ -0,0 +1,16 @@
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author coderpwh
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/coderpwh/syntaxexamples/agent/CompletionAgent.java
@@ -0,0 +1,61 @@
+package com.coderpwh.syntaxexamples.agent;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.core.credential.AzureKeyCredential;
+import com.microsoft.semantickernel.plugin.KernelPlugin;
+import com.microsoft.semantickernel.plugin.KernelPluginFactory;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
+//import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
+
+/**
+ * @author coderpwh
+ */
+
+
+public class CompletionAgent {
+
+    public static void main(String[] args) {
+
+        String AZURE_OPENAI_API_KEY = "";
+        String AZURE_OPENAI_API_ENDPOINT = "";
+        String AZURE_OPENAI_API_VERSION = "2023-05-15";
+        String MODEL_ID = "";
+
+
+        OpenAIAsyncClient client = new OpenAIClientBuilder().credential(new AzureKeyCredential(AZURE_OPENAI_API_KEY))
+                .endpoint(AZURE_OPENAI_API_ENDPOINT).buildAsyncClient();
+
+
+
+
+  /*      ChatCompletionService chatCompletion = OpenAIChatCompletion.builder()
+                .withModelId(MODEL_ID)
+                .withOpenAIAsyncClient(client)
+                .build();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
