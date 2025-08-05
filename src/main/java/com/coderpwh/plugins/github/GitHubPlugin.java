3c4435ab570afbea38060f44007a0a65c979229e
pengwenhao <pengwenhao@aliyun.com>
2025-08-05 11:44:16 +0800

    feat:init github

---
+++ src/main/java/com/coderpwh/plugins/github/GitHubPlugin.java
@@ -0,0 +1,59 @@
+package com.coderpwh.plugins.github;
+
+import com.microsoft.semantickernel.semanticfunctions.annotations.KernelFunctionParameter;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+public class GitHubPlugin {
+
+
+    public static  final String baseUrl = "https://api.github.com";
+
+    private final  String token;
+
+    public GitHubPlugin(String token) {
+        this.token = token;
+    }
+
+
+    /***
+     * 访问github http工具
+     * @return
+     */
+    private HttpClient createClient() {
+        return HttpClient.create()
+                .baseUrl(baseUrl)
+                .headers(headers -> {
+                    headers.add("User-Agent", "request");
+                    headers.add("Accept", "application/vnd.github+json");
+                    headers.add("Authorization", "Bearer " + token);
+                    headers.add("X-GitHub-Api-Version", "2022-11-28");
+                });
+    }
+
+
+    private static  String builderQueryString(String path,String param,String value){
+        if(value==null||value.isEmpty()||value.equals(KernelFunctionParameter.NO_DEFAULT_VALUE)){
+            return  path;
+        }
+        return path + (path.contains("?") ? "&" : "?") + param + "=" + value;
+    }
+
+
+
+    private Mono<String> makeRequest(HttpClient client, String path){
+        return  client.get()
+                .uri(path)
+                .responseSingle((res,content)->{
+                   if(res.status().code()!=200){
+                       return Mono.error(new IllegalStateException("Request failed: " + res.status()));
+                   }
+                   return  content.asString();
+                });
+    }
+
+
+
+
+
+}
--- src/main/java/com/coderpwh/syntaxexamples/agent/CompletionAgent.java
+++ src/main/java/com/coderpwh/syntaxexamples/agent/CompletionAgent.java
@@ -3,6 +3,7 @@ package com.coderpwh.syntaxexamples.agent;
 import com.azure.ai.openai.OpenAIAsyncClient;
 import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.core.credential.AzureKeyCredential;
+import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.plugin.KernelPlugin;
 import com.microsoft.semantickernel.plugin.KernelPluginFactory;
 import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
@@ -31,6 +32,11 @@ public class CompletionAgent {
                 .withModelId(MODEL_ID)
                 .withOpenAIAsyncClient(client)
                 .build();
+
+
+/*        Kernel kernel = Kernel.builder()
+                .withAIService(ChatCompletionService.class, chatCompletion)*/
+
 
 
     }
