d8a7d2e126c8e445981439762a184ff5ce5d319a
pengwenhao <pengwenhao@aliyun.com>
2025-08-06 11:03:38 +0800

    fix:update CustomTypes_Example

--- src/main/java/com/coderpwh/syntaxexamples/java/CustomTypes_Example.java
+++ src/main/java/com/coderpwh/syntaxexamples/java/CustomTypes_Example.java
@@ -1,9 +1,15 @@
 package com.coderpwh.syntaxexamples.java;
 
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.core.credential.AzureKeyCredential;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
 import com.microsoft.semantickernel.contextvariables.ContextVariableTypeConverter;
+import com.microsoft.semantickernel.contextvariables.ContextVariableTypes;
+import com.microsoft.semantickernel.contextvariables.converters.ContextVariableJacksonConverter;
 import com.microsoft.semantickernel.semanticfunctions.KernelArguments;
 import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
 
@@ -17,6 +23,24 @@ public class CustomTypes_Example {
 
     public static void main(String[] args) {
 
+        String AZURE_OPENAI_API_KEY = "";
+        String AZURE_OPENAI_API_ENDPOINT = "";
+        String MODEL_ID = "";
+
+
+        OpenAIAsyncClient client = new OpenAIClientBuilder().credential(new AzureKeyCredential(AZURE_OPENAI_API_KEY))
+                .endpoint(AZURE_OPENAI_API_ENDPOINT)
+                .buildAsyncClient();
+
+        ChatCompletionService chatCompletionService = OpenAIChatCompletion.builder()
+                .withOpenAIAsyncClient(client)
+                .withModelId(MODEL_ID)
+                .build();
+
+
+        exampleBuildingCustomConverter(chatCompletionService);
+        exampleUsingJackson(chatCompletionService);
+        exampleUsingGlobalTypes(chatCompletionService);
     }
 
 
@@ -64,6 +88,40 @@ public class CustomTypes_Example {
 
         Pet updated = kernel.invokePromptAsync("Change Sandy's name to Daisy:\\n{{$Sandy}}", KernelArguments.builder()
                 .withVariable("Sandy", sandy, typeConverter).build()).withTypeConverter(typeConverter).withResultType(Pet.class).block().getResult();
+
+        System.out.println("Sandy's updated record: " + updated);
+    }
+
+    public static void exampleUsingJackson(ChatCompletionService chatCompletionService) {
+
+        Pet sandy = new Pet("Sandy", 3, "Dog");
+
+        Kernel kernel = Kernel.builder().withAIService(ChatCompletionService.class, chatCompletionService).build();
+
+        ContextVariableTypeConverter<Pet> typeConverter = ContextVariableJacksonConverter.create(Pet.class);
+
+        Pet updated = kernel.invokePromptAsync("Increase Sandy's age by a year:\n{{$Sandy}}",
+                KernelArguments.builder()
+                        .withVariable("Sandy", sandy, typeConverter)
+                        .build()).withTypeConverter(typeConverter).withResultType(Pet.class).block().getResult();
+
+        System.out.println("Sandy's updated record: " + updated);
+    }
+
+
+    public static void exampleUsingGlobalTypes(ChatCompletionService chatCompletionService) {
+
+        Pet sandy = new Pet("Sandy", 3, "Dog");
+
+        Kernel kernel = Kernel.builder().withAIService(ChatCompletionService.class, chatCompletionService).build();
+
+        ContextVariableTypeConverter<Pet> typeConverter = ContextVariableJacksonConverter.create(Pet.class);
+        ContextVariableTypes.addGlobalConverter(typeConverter);
+
+
+        Pet updated = kernel.invokePromptAsync("Sandy's is actually a cat correct this:\n{{$Sandy}}", KernelArguments.builder()
+                .withVariable("Sandy", sandy)
+                .build()).withResultType(Pet.class).block().getResult();
 
         System.out.println("Sandy's updated record: " + updated);
     }
