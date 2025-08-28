2259fb717b87c1a45d56aff5f60666e6d96f4040
pengwenhao <pengwenhao@aliyun.com>
2025-08-28 11:02:44 +0800

    fix:update example08

---
+++ src/main/java/com/coderpwh/syntaxexamples/configuration/Example08_RetryHandler.java
@@ -0,0 +1,65 @@
+package com.coderpwh.syntaxexamples.configuration;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.http.policy.ExponentialBackoffOptions;
+import com.azure.core.http.policy.RetryOptions;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
+import com.microsoft.semantickernel.semanticfunctions.KernelFunction;
+import com.microsoft.semantickernel.semanticfunctions.KernelFunctionFromPrompt;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
+
+import java.time.Duration;
+
+public class Example08_RetryHandler {
+
+
+    private static String AZURE_OPENAI_API_KEY = " ";
+    private static String AZURE_OPENAI_API_ENDPOINT = "";
+    private static String MODEL_ID = "";
+
+    public static void main(String[] args) {
+
+        RetryOptions  retryOptions = new RetryOptions(new ExponentialBackoffOptions()
+                .setMaxDelay(Duration.ofSeconds(10))
+                .setBaseDelay(Duration.ofSeconds(2))
+                .setMaxRetries(3));
+
+
+        OpenAIAsyncClient client = new OpenAIClientBuilder().retryOptions(retryOptions)
+                .endpoint(AZURE_OPENAI_API_ENDPOINT)
+                .credential(new AzureKeyCredential(AZURE_OPENAI_API_KEY))
+                .buildAsyncClient();
+
+        ChatCompletionService  openAIChatCompletion = OpenAIChatCompletion.builder()
+                .withOpenAIAsyncClient(client)
+                .withModelId(MODEL_ID)
+                .build();
+
+
+        Kernel kernel = Kernel.builder()
+                .withAIService(ChatCompletionService.class, openAIChatCompletion)
+                .build();
+
+        String question = "How popular is the Polly library?";
+
+
+        KernelFunction<String> function = KernelFunctionFromPrompt.<String>builder()
+                .withTemplate(question)
+                .build();
+
+        try {
+             var result = kernel.invokeAsync(function).block();
+            System.out.println("结果为:"+result);
+        }catch (Exception e){
+            System.out.println("重试");
+        }
+
+
+
+    }
+
+
+}
