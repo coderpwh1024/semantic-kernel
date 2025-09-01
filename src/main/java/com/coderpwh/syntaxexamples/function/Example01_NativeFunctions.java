e2f0c9e278b943edb07bde813f86a993dbfaa2c3
pengwenhao <pengwenhao@aliyun.com>
2025-09-01 14:01:22 +0800

    feat:init example 01

--- src/main/java/com/coderpwh/syntaxexamples/function/Example01_NativeFunctions.java
+++ src/main/java/com/coderpwh/syntaxexamples/function/Example01_NativeFunctions.java
@@ -1,8 +1,19 @@
 package com.coderpwh.syntaxexamples.function;
 
+import com.coderpwh.plugins.text.TextPlugin;
+
 public class Example01_NativeFunctions {
 
     public static void main(String[] args) {
+
+        TextPlugin textPlugin = new TextPlugin();
+
+//        String result = textPlugin.concat("Hello", "World");
+
+        // Use function without kernel
+        String result = textPlugin.uppercase("ciao!");
+
+        System.out.println(result);
 
     }
 }
