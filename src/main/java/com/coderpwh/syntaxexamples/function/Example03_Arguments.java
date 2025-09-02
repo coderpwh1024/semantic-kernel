ec300dde8c5d23eccea6d4b5c0d6272b834af68b
pengwenhao <pengwenhao@aliyun.com>
2025-09-02 12:41:54 +0800

    fix:update Example03_arguments

--- src/main/java/com/coderpwh/syntaxexamples/function/Example03_Arguments.java
+++ src/main/java/com/coderpwh/syntaxexamples/function/Example03_Arguments.java
@@ -1,5 +1,10 @@
 package com.coderpwh.syntaxexamples.function;
 
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.orchestration.FunctionResult;
+import com.microsoft.semantickernel.plugin.KernelPlugin;
+import com.microsoft.semantickernel.plugin.KernelPluginFactory;
+import com.microsoft.semantickernel.semanticfunctions.KernelArguments;
 import com.microsoft.semantickernel.semanticfunctions.annotations.DefineKernelFunction;
 import com.microsoft.semantickernel.semanticfunctions.annotations.KernelFunctionParameter;
 import reactor.core.publisher.Mono;
@@ -11,11 +16,23 @@ public class Example03_Arguments {
 
     public static void main(String[] args) {
 
+        Kernel kernel = Kernel.builder().build();
+
+        KernelPlugin kernelFunctions = KernelPluginFactory.createFromObject(new StaticTextPlugin(),"text");
+
+        KernelArguments arguments = KernelArguments.builder().withInput("Today is:").withVariable("day","Monday").build();
+
+
+        FunctionResult<?> resultValue = kernel.invokeAsync(kernelFunctions.get("AppendDay")).withArguments(arguments).block();
+        System.out.println(resultValue.getResult());
+
     }
 
 
+    /***
+     * 静态插件
+     */
     public static class StaticTextPlugin {
-
 
         /***
          * 静态方法
@@ -28,9 +45,16 @@ public class Example03_Arguments {
         }
 
 
-        public Mono<String> appendDay() {
-            return  Mono.just("");
-
+        /***
+         * 动态方法
+         * @param input
+         * @param day
+         * @return
+         */
+        @DefineKernelFunction(description = "Append current day to a string", name = "appendDay", returnType = "java.lang.String")
+        public Mono<String> appendDay(@KernelFunctionParameter(description = "Text to append to", name = "input") String input,
+                                      @KernelFunctionParameter(description = "Current day", name = "day") String day) {
+            return Mono.just(input + day);
         }
 
     }
