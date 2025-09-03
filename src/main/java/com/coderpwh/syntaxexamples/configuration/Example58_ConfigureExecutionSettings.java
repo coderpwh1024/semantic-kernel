c2e0790bbb793d6a4db60ed97285a88dee41e835
pengwenhao <pengwenhao@aliyun.com>
2025-09-03 13:10:33 +0800

    fix:update example 58

--- src/main/java/com/coderpwh/syntaxexamples/configuration/Example58_ConfigureExecutionSettings.java
+++ src/main/java/com/coderpwh/syntaxexamples/configuration/Example58_ConfigureExecutionSettings.java
@@ -14,9 +14,9 @@ import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionServic
 
 public class Example58_ConfigureExecutionSettings {
 
-    private static String AZURE_OPENAI_API_KEY = " ";
+    private static String AZURE_OPENAI_API_KEY = "";
     private static String AZURE_OPENAI_API_ENDPOINT = "";
-    private static String MODEL_ID = " ";
+    private static String MODEL_ID = "";
 
 
     public static void main(String[] args) {
